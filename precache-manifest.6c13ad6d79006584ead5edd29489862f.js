bf7ce6affe1fccd95d681b474be9d4320f644824
Code Sandbox <hello@codesandbox.io>
2022-12-31 04:21:50 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.6c13ad6d79006584ead5edd29489862f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70baac4836c72e22dac9c66e3c235bd7",
+    "url": "https://vijaychari.github.io/csb-0kziyw/index.html"
+  },
+  {
+    "revision": "61aefb9691b71d6cdb03",
+    "url": "https://vijaychari.github.io/csb-0kziyw/static/css/main.99fc6a73.chunk.css"
+  },
+  {
+    "revision": "233c728184e7ead75061",
+    "url": "https://vijaychari.github.io/csb-0kziyw/static/js/2.8083575e.chunk.js"
+  },
+  {
+    "revision": "464fef3691779b3926634327df24dae8",
+    "url": "https://vijaychari.github.io/csb-0kziyw/static/js/2.8083575e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61aefb9691b71d6cdb03",
+    "url": "https://vijaychari.github.io/csb-0kziyw/static/js/main.c90d9eaa.chunk.js"
+  },
+  {
+    "revision": "0d963af45bf2c81726cd",
+    "url": "https://vijaychari.github.io/csb-0kziyw/static/js/runtime-main.302b8418.js"
+  },
+  {
+    "revision": "789f86e41cb7062ee56b6fe8654b8653",
+    "url": "https://vijaychari.github.io/csb-0kziyw/static/media/girl.789f86e4.png"
+  }
+]);
